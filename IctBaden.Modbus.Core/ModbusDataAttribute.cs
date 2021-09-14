5f736a07666592323681344de577ef0559e34311
Frank Pfattheicher <fpf@ict-baden.de>
2021-09-14 11:16:48 +0200

    GetValue, Mapper ToType

---
+++ IctBaden.Modbus.Core/ModbusDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IctBaden.Modbus.Core
+{
+    public class ModbusDataAttribute : Attribute
+    {
+        public readonly ModbusDataType Type;
+        public readonly ModbusDataFormat Format;
+
+        public ModbusDataAttribute(ModbusDataType type, ModbusDataFormat format)
+        {
+            Type = type;
+            Format = format;
+        }
+    }
+}
